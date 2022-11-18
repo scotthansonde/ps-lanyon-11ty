77f5295d72cec4f833540ccc7cdcd5febaeb9cea
Scott Hanson <papascott@gmail.com>
2022-11-18 18:47:27 +0100

    Update url in function

--- functions/deploy-succeeded/deploy-succeeded.js
+++ functions/deploy-succeeded/deploy-succeeded.js
@@ -3,10 +3,7 @@ const fetch = require("node-fetch");
 const handler = async function () {
   try {
     const params = new URLSearchParams();
-    params.append(
-      "url",
-      "https://astonishing-starlight-2035d3.netlify.app/feed.xml"
-    );
+    params.append("url", "https://scotthanson.de/feed.xml");
 
     const response = await fetch("http://rpc.rsscloud.io:5337/ping", {
       method: "POST",
