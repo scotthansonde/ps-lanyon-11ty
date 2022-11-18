d3db9691165596d1528b2f85e4d07a9631e71ed2
Scott Hanson <papascott@gmail.com>
2022-11-18 11:52:08 +0100

    Ready for GitHub

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,22 @@
-const { readFileSync } = require('fs');
-const { DateTime } = require('luxon');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
+const { readFileSync } = require("fs");
+const { DateTime } = require("luxon");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 function format(date, str) {
-  return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(str);
+  return DateTime.fromJSDate(date, { zone: "utc" }).toFormat(str);
 }
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addFilter('dateToString', (date) => {
-    return format(date, 'dd LLL yyyy'); // 09 Oct 1986
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("dateToString", (date) => {
+    return format(date, "dd LLL yyyy"); // 09 Oct 1986
   });
-  eleventyConfig.addFilter('htmlDateString', (date) => {
-    return format(date, 'yyyy-LL-dd'); // 1986-10-09
+  eleventyConfig.addFilter("htmlDateString", (date) => {
+    return format(date, "yyyy-LL-dd"); // 1986-10-09
   });
-  eleventyConfig.addFilter('dateToFolders', (date) => {
-    return format(date, 'yyyy/LL/dd'); // 1986/10/09
+  eleventyConfig.addFilter("dateToFolders", (date) => {
+    return format(date, "yyyy/LL/dd"); // 1986/10/09
   });
-  eleventyConfig.addFilter('latest', (posts, num = 3, skip = '') => {
+  eleventyConfig.addFilter("latest", (posts, num = 3, skip = "") => {
     const latest = [];
     for (const post of posts) {
       if (post.url != skip) {
@@ -26,8 +26,14 @@ module.exports = function(eleventyConfig) {
     }
     return latest;
   });
+  eleventyConfig.addFilter("post_permalink", (page) => {
+    const yyyy = page.date.getFullYear();
+    const mm = String(page.date.getMonth() + 1).padStart(2, "0");
+    const dd = String(page.date.getDate()).padStart(2, "0");
+    return `${yyyy}/${mm}/${dd}/${page.fileSlug}/`;
+  });
 
-  for (const f of ['css', 'public']) {
+  for (const f of ["css", "public"]) {
     eleventyConfig.addPassthroughCopy(f);
   }
 
@@ -35,15 +41,15 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = readFileSync('_site/404.html');
+      ready: function (err, browserSync) {
+        const content_404 = readFileSync("_site/404.html");
 
-        browserSync.addMiddleware('*', (req, res) => {
+        browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
           res.write(content_404);
           res.end();
         });
-      }
-    }
+      },
+    },
   });
 };
